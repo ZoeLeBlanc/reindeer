43fd1ce54f6a01ba9fdeb21330fd597a2d0d04fc
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2016-07-20 17:36:26 -0500

    updates

---
+++ reindeer.js
@@ -0,0 +1,15 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+
+var hohohoElement = document.getElementById("reindeer");
+
+var coloredReindeer = '';
+
+for (var i = 0; i < reindeer.length; i++){
+	var coloredReindeer =  colors[i] + ' ' + reindeer[i];
+	console.log(coloredReindeer);
+	hohohoElement.innerHTML += "<div>" + colors[i] + ' ' + reindeer[i] + "</div>";
+}
+
+
